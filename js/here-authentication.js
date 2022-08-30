6c7a9d4eff64ac7e30880353f3c8235729fd4b6b
Shoney John <shoney.john@outlook.com>
2022-08-31 01:38:30 +0200

    import Oauth dependency lib

---
+++ js/here-authentication.js
@@ -0,0 +1,72 @@
+//Import the below modules using "npm i -save request oauth-1.0a crypto"
+import OAuth from "./oauth-1.0a";
+// Call the API
+// This is a POST request, because we need the API to generate a new token for us
+fetch('https://account.api.here.com/oauth2/token', {
+    method: 'POST',
+    body: 'grant_type=client_credentials',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'OAuth oauth_consumer_key="39KFTkbvunKUUTmzhZ7x1Q",oauth_signature_method="HMAC-SHA256",oauth_timestamp="1661900126",oauth_nonce="kyA3BXD0Il1",oauth_version="1.0",oauth_signature="3R%2F4KVkPexdlqxCvKZgBEgxuyg5Y8eraZypz%2FRl7cIk%3D"'
+
+    }
+}).then(function (resp) {
+
+    // Return the response as JSON
+    return resp.json();
+
+}).then(function (data) {
+
+    // Log the API data
+    console.log('token', data);
+
+}).catch(function (err) {
+
+    // Log any errors
+    console.log('something went wrong', err);
+
+});
+
+// // Token request function
+// function generateToken() {
+//     // #1 Initialize OAuth with your HERE OAuth credentials from the credentials file that you downloaded above
+//     const oauth = OAuth({
+//         consumer: {
+//             key: 'your_key', //Access key
+//             secret: 'your_secret_key', //Secret key
+//         },
+//         signature_method: 'HMAC-SHA256',
+//         hash_function(base_string, key) {
+//             return crypto
+//                 .createHmac('sha256', key)
+//                 .update(base_string)
+//                 .digest('base64')
+//         },
+//     });
+//     // #2 Building the request object.
+//     const request_data = {
+//         url: 'https://account.api.here.com/oauth2/token',
+//         method: 'POST',
+//         data: { grant_type: 'client_credentials' },
+//     };
+//     // #3 Sending the request to get the access token
+//     request(
+//         {
+//             url: request_data.url,
+//             method: request_data.method,
+//             form: request_data.data,
+//             headers: oauth.toHeader(oauth.authorize(request_data)),
+//         },
+//         function (error, response, body) {
+
+//             if (response.statusCode == 200) {
+//                 result = JSON.parse(response.body);
+//                 console.log('Token', result);
+//             }
+//         }
+//     );
+// }
+
+// // Calling this function to get the access token
+
+// generateToken();
--- readme.md
+++ readme.md
@@ -4,5 +4,8 @@
 docker-compose up -d
 ```
 
+### OAuth
 
-
+https://cdnjs.com/libraries/crypto-js
+https://developer.here.com/tutorials/how-to-authenticate-with-here-oauth/
+https://www.npmjs.com/package/oauth-1.0a
